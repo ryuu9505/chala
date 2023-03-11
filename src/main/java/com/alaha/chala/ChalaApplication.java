41fa53f7c7e0b6306ef2670627754ed31f507978
ryuu <ryuu.public@gmail.com>
2023-03-12 08:06:17 +0900

    first init

---
+++ src/main/java/com/alaha/chala/ChalaApplication.java
@@ -0,0 +1,13 @@
+package com.alaha.chala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChalaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alaha/chala/ChalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaha.chala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
