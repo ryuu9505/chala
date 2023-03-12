a18b32e61b184d944337386d59a08fe170175f81
ryuu <ryuu.public@gmail.com>
2023-03-12 19:35:38 +0900

    feat: #4 update domains as a JPA entity
    - Set @Entity
    - Mapping Post and Comment
    - Auditing duplicated fields
    - Generate equals( ) and hashCode( )
    - Indexing
    - etc. set field details

--- src/main/java/com/alaha/chala/domain/Comment.java
+++ src/main/java/com/alaha/chala/domain/Comment.java
@@ -1,14 +1,77 @@
 package com.alaha.chala.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Comment {
-    private Long id;
-    private Post post;
-    private String content;
 
-    private LocalDateTime createAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @Column(nullable = false, length = 400) private String content;
+
+    @ManyToOne(optional = false)
+    private Post post;
+
+    @CreatedDate
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
     private String createdBy;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    protected Comment() {}
+
+    private Comment(String content, Post post) {
+        this.content = content;
+        this.post = post;
+    }
+
+    public static Comment of(String content, Post post) {
+        return new Comment(content, post);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment comment)) return false;
+        return id != null && id.equals(comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
