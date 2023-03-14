670847b9b0bdbf7e837c3b7c5a4ecdc4114162e0
ryuu <ryuu.public@gmail.com>
2023-03-14 15:22:11 +0900

    feat: add @RepositoryRestResource for the repository classes

--- src/main/java/com/alaha/chala/repository/CommentRepository.java
+++ src/main/java/com/alaha/chala/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.alaha.chala.repository;
 
 import com.alaha.chala.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
--- src/main/java/com/alaha/chala/repository/PostRepository.java
+++ src/main/java/com/alaha/chala/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.alaha.chala.repository;
 
 import com.alaha.chala.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
