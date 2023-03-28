a433f177ba35f270ccdbd5384da081882ec3fff4
ryuu <ryuu.public@gmail.com>
2023-03-29 06:59:48 +0900

    feat: #16 auditing for entity classes

--- src/main/java/com/alaha/chala/domain/UserAccount.java
+++ src/main/java/com/alaha/chala/domain/UserAccount.java
@@ -4,31 +4,21 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
 @ToString
 @Table(indexes = {
-        @Index(columnList = "username"),
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
 })
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class UserAccount {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class UserAccount extends AuditingFields {
 
-    @Setter
+    @Id
     @Column(nullable = false, length = 50)
     private String username;
     @Setter
@@ -46,37 +36,36 @@ public class UserAccount {
     @Setter
     private String memo;
 
-    // todo: mapping
-
-    @CreatedDate
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
     protected UserAccount() {}
 
-    private UserAccount(String username, String password, String email, String nickname, String memo) {
+    private UserAccount(String username, String password, String email, String nickname, String memo, String createdBy) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;
     }
 
-    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+    public static UserAccount of(String username, String password, String email, String nickname, String memo) {
+        return UserAccount.of(username, password, email, nickname, memo, null);
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof UserAccount userAccount)) return false;
-        return id != null && id.equals(userAccount.id);
+        return this.getUsername() != null && this.getUsername().equals(userAccount.getUsername());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(this.getUsername());
     }
 
 }
