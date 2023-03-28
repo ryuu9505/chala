251cf82dcf46bf168a682e9cfdb59ee3a78a3159
ryuu <ryuu.public@gmail.com>
2023-03-29 06:50:57 +0900

    feat: add configuration for thymeleaf

---
+++ src/main/java/com/alaha/chala/config/ThymeleafConfig.java
@@ -0,0 +1,14 @@
+package com.alaha.chala.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+}
